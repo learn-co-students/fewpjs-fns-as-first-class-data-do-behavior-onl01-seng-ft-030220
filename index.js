074cd7ba44b5f71d26b916740855e22094459bfa
pharia-le <pharia.le@gmail.com>
2020-06-07 23:59:12 -0700

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(input) {
+  let hr = parseInt(input.split(":")[0])
+  if (hr < 12) {
+    return "Good Morning"
+  } else if (hr < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+};
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(input) {
+  document.getElementById('greeting').innerText = input
+}
