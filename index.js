70c24c50d0002374b9621d4af1c140ab51560014
rgonzalez12 <raymondalexander.gonzalez@gmail.com>
2020-06-08 21:22:49 -0600

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-/* Given Code, don't edit */
-
 document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
@@ -7,7 +5,14 @@ function handleClick(e) {
   displayMessage(greet(timeString))
 }
 
-/* End Given Code, don't edit above here...*/
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message
+}
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(x) {
+  const clockTime = parseInt(x, 10);
+
+  if (clockTime < 12) return "Good Morning"
+  if (clockTime > 17) return "Good Evening"
+  return "Good Afternoon"
+}
