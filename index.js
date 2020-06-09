9e3a2021a268ba1dad352a59c94554617a44a130
Dusch4593 <brandondusch@gmail.com>
2020-06-09 13:23:17 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let time_array = time.split(":")
+  let hours = parseInt(time_array[0])
+
+  if(hours > 17) {
+    return "Good Evening"
+  } else {
+    if(hours <= 12) {
+      return "Good Morning"
+    } else {
+      return "Good Afternoon"
+    }
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  let greeting = document.getElementById("greeting");
+  greeting.innerText = message;
+}
